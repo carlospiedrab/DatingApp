647fdd1ed0442e49af9b44b19d1cb2a9dc22f2ae
Carlos Piedra <barce90@hotmail.com>
2024-07-11 16:48:35 -0700

    End of Section 1&2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly DataContext _db;
+
+        public UsersController(DataContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _db.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _db.Users.FindAsync(id);
+            if(user == null) return NotFound("Usuario no encontrado");
+            return Ok(user);
+        }
+    }
+}
